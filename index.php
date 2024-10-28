49e6f6f3cc34eabe2a25dfdc504daaba434c7ce4
aldiwebprogreming <davinal1956@gmail.com>
2024-10-28 10:18:06 +0700

    nusadigital

---
+++ index.php
@@ -0,0 +1,293 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nusadigital</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<style>
+    .container-fluid {
+        background-image: url('https://static.vecteezy.com/system/resources/previews/024/449/746/non_2x/colorful-purple-shapes-and-abstract-background-design-in-the-style-of-rounded-shapes-dark-pink-animated-gifs-simple-shapes-soft-edged-linear-generate-ai-free-photo.jpg');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        position: relative;
+    }
+    .container-fluid::before {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background: rgba(0, 0, 0, 0.6); /* Latar transparan */
+        z-index: 1;
+    }
+    .text-center {
+        z-index: 2;
+    }
+
+    .content1 {
+       background-image: url('https://png.pngtree.com/background/20210715/original/pngtree-pink-colorful-gradient-background-picture-image_1283704.jpg');
+       background-size: cover;
+       background-repeat: no-repeat;
+       background-position: center;
+       position: relative;
+       padding-top: 40px;
+       padding-bottom: 30px;
+
+   }
+   .content2 {
+    background-color: #2b2e5a;
+    padding-top : 40px;
+    padding-bottom: 30px;
+}
+.card-custom{
+    background-color: #2b2e5a;
+}
+
+.content3{
+ background-image: url('https://png.pngtree.com/background/20210715/original/pngtree-pink-colorful-gradient-background-picture-image_1283704.jpg');
+ background-size: cover;
+ background-repeat: no-repeat;
+ background-position: center;
+ position: relative;
+ padding-top: 40px;
+ padding-bottom: 30px;
+
+}
+</style>
+
+
+<body>
+
+    <div class="container-fluid bg-dark text-white vh-100 d-flex align-items-center justify-content-center">
+        <div class="text-center">
+            <h1 class="display-3 fw-bold">Selamat Datang di Nusadigital</h1>
+            <p class="lead">Buat kebutuhan website usaha anda di nusadigital</p>
+            <a href="#" class="btn border-danger text-white btn-lg mt-4 rounded-pill">Mulai Sekarang</a>
+        </div>
+    </div>
+
+    <div class="content1">
+        <div class="container">
+            <div class="section-header">
+                <small class="text-uppercase btn border-dark">Our Expertise</small>
+                <h2 class="mt-2 text-danger fw-bold">Layanan Unggulan Nusadigital untuk Kamu</h2>
+                <p class="text-danger">Hasil Website yang kami buat sudah dilengkapi berbagai fitur yang dibutuhkan untuk keperluan Digital Marketing di tahun 2024 ini</p>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-4 col-12 mt-4">
+                    <div class="card shadow">
+                      <img src="https://jasterweb.com/wp-content/uploads/2023/11/webx.jpg" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h5 class="card-title">Website Bisnis / Jualan</h5>
+
+                        <p class="fw-bold text-danger">Mulai Rp 1.500.000</p>
+                        <hr />
+                        <div class="text-secondary">
+                           <i class="fas fa-angle-right"></i> Website siap pakai
+                       </div>
+                       <hr />
+                       <div class="text-secondary">
+                          <i class="fas fa-angle-right"></i> Gratis Domain & Hosting 1 Tahun
+                      </div>
+                      <hr />
+                      <div class="text-secondary">
+                          <i class="fas fa-angle-right"></i> Bisa Request Tampilan
+                      </div>
+                  </div>
+              </div>
+          </div>
+
+          <div class="col-sm-4 col-12 mt-4">
+            <div class="card shadow">
+                <img src="https://jasterweb.com/wp-content/uploads/2023/11/customx.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Website Custom</h5>
+                    <p class="fw-bold text-danger">Mulai Rp 2.000.000</p>
+                    <hr/>
+                    <div class="text-secondary">
+                        <i class="fas fa-angle-right"></i>  Cocok untuk kebutuhan Internal
+                    </div>
+                    <hr />
+                    <div class="text-secondary">
+                       <i class="fas fa-angle-right"></i> Fitur menyesuaikan Kebutuhan
+                   </div>
+                   <hr />
+                   <div class="text-secondary">
+                    <i class="fas fa-angle-right"></i> User Friendly & Responsive
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="col-sm-4 col-12 mt-4">
+      <div class="card shadow">
+          <img src="https://jasterweb.com/wp-content/uploads/2023/11/redx.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Jasa Redesign Website</h5>
+            <p class="fw-bold text-danger">Mulai Rp 1.000.000</p>
+            <hr/>
+            <div class="text-secondary">
+               <i class="fas fa-angle-right"></i> Tidak Merubah Domain
+           </div>
+           <hr />
+           <div class="text-secondary">
+              <i class="fas fa-angle-right"></i> Tampilan Bisa Request
+          </div>
+          <hr />
+          <div class="text-secondary">
+            <i class="fas fa-angle-right"></i> Bonus Optimasi Loading Speed
+        </div>
+    </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<div>
+    <div class="content2">
+        <div class="container">
+            <div class="section-header">
+                <small class="text-uppercase btn border-white text-white">Why Choose Us ?</small>
+                <h2 class="mt-2 text-white fw-bold">Keuntungan Menggunakan
+                Layanan Jasterweb</h2>
+                <p class="text-white">Karena Website bukan hanya soal bagus atau tidaknya, Tapi bagaimana cara maintenancenya untuk Perkembangan Bisnis Kamu</p>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-4 col-12 mt-3">
+                    <div class="card card-custom border-white">
+                        <div class="card-body text-white">
+                            <div class="d-flex">
+                                <img src="https://jasterweb.com/wp-content/uploads/2023/11/i-layanan.png" class="img-fluid" alt="" style="height: 80px;">
+                                <h5 class="mt-4" style="margin-left: 20px;">Layanan All in One</h5>
+                            </div>
+                            <p class="mt-3">Tak hanya Website, Jasterweb juga bisa membuatkan kamu Design Promosi & Skalian Iklan di Google</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-12 mt-3">
+                    <div class="card card-custom border-white">
+                        <div class="card-body text-white">
+                            <div class="d-flex">
+                                <img src="https://jasterweb.com/wp-content/uploads/2023/11/i-harga.png" class="img-fluid" alt="" style="height: 80px;">
+                                <h5 class="mt-4" style="margin-left: 20px;">Harga Bisa Request</h5>
+                            </div>
+                            <p class="mt-3">Suka sama Jasterweb tapi Harga belum cocok ? Santai. Kamu bisa tentukan harga websitemu sendiri</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-12 mt-3">
+                    <div class="card card-custom border-white">
+                        <div class="card-body text-white">
+                            <div class="d-flex">
+                                <img src="https://jasterweb.com/wp-content/uploads/2023/11/i-custom.png" class="img-fluid" alt="" style="height: 80px;">
+                                <h5 class="mt-4" style="margin-left: 20px;">Ready to Customs</h5>
+                            </div>
+                            <p class="mt-3">Selain Web Company & Toko Online, Jasterweb juga siap untuk membuat Website Kompleks / Custom</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-12 mt-3">
+                    <div class="card card-custom border-white">
+                        <div class="card-body text-white">
+                            <div class="d-flex">
+                                <img src="https://jasterweb.com/wp-content/uploads/2023/11/i-custom.png" class="img-fluid" alt="" style="height: 80px;">
+                                <h5 class="mt-4" style="margin-left : 20px">Alur Pekerjaan Jelas</h5>
+                            </div>
+                            <p class="mt-3">Kami Memiliki Cara Kerja yang Efisien dan Profesional untuk membuat websitemu Go-Online dengan sangat baik</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4 col-12 mt-3">
+                    <div class="card card-custom border-white">
+                        <div class="card-body text-white">
+                            <div class="d-flex">
+                                <img src="https://jasterweb.com/wp-content/uploads/2023/11/i-garansi.png" class="img-fluid" alt="" style="height: 80px;">
+                                <h5 class="mt-4" style="margin-left : 20px">Garansi Selamanya</h5>
+                            </div>
+                            <p class="mt-3">Tak perlu khawatir soal Support atau Maintenance, Jasterweb siap beri Garansi Website Seumur Hidup.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4 col-12 mt-3">
+                    <div class="card card-custom border-white">
+                        <div class="card-body text-white">
+                            <div class="d-flex">
+                                <img src="https://jasterweb.com/wp-content/uploads/2023/11/i-bonus.png" class="img-fluid" alt="" style="height: 80px;">
+                                <h5 class="mt-4" style="margin-left : 20px">Banyak Bonus Gratis</h5>
+                            </div>
+                            <p class="mt-3">Disetiap Paket Website. Jasterweb akan memberikan Bonus yang sangat berguna buat bisnismu</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div>
+    <div class="content3">
+        <div class="container">
+            <div class="mt-3">
+                <h4 class="text-danger fw-bold">Kontak Yang Dapat Anda Hubungi</h4>
+                <p class="text-danger">Anda dapat menghubungi kami dengan kontak di bawah ini</p>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-6 col-12 mt-3">
+                    <div class="card shadow">
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label class="fw-bold">
+                                    Email 
+                                </label>
+                                <p class="text-secondary fw-bold">Nusadigital96@gmail.com</p>
+                            </div>
+                            <hr />
+
+                            <div class="form-group">
+                                <label class="fw-bold">
+                                    Whatsapp
+                                </label>
+                                <p class="text-secondary fw-bold">+6283138184143</p>
+                            </div>
+                            <hr />
+                            <div class="form-group">
+                                <label class="fw-bold">
+                                    Alamat
+                                </label>
+                                <p class="fw-bold text-secondary">Perumahan Azzahra No.2 Kelurahan Stabat Baru, Kecamatan Stabat, Kabupaten Langkat Sumatera Utara</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-12 mt-3">
+                    <div class="card">
+                        <div class="card-body">
+                         <div class="ratio ratio-16x9">
+                             <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.187426999262!2d98.46225257396308!3d3.769356949029254!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30372bc3f574d801%3A0x8b42df9a0d48ab5c!2sPERUMAHAN%20AZ-ZAHRA%203!5e0!3m2!1sen!2sid!4v1729925148511!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>
+
+
+
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
